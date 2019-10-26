aa80357b5deace8a525fbc3f863a4976e3c8282b
Marco Hernandez <marcohern@gmail.com>
2019-10-26 08:47:14 -0500

    added tilemap sample

---
+++ assets/tilemaps/tiles1.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tiles1" tilewidth="128" tileheight="128" tilecount="400" columns="5">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../tiles/tiles1.png" width="640" height="640"/>
+</tileset>
