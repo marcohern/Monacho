2e284b16c7fbd6f19451e8d56dbaf5926b707cb6
Marco Hernandez <marcohern@gmail.com>
2019-10-26 09:12:54 -0500

    remove tile seams

---
+++ assets/tilemaps/tiles1.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tiles1" tilewidth="128" tileheight="128" tilecount="400" columns="5">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../tiles/tiles1.png" width="640" height="640"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="4" y="0" width="124" height="127"/>
+  </objectgroup>
+ </tile>
+</tileset>
